91752866e8c64cf9e23c6dcc5f52cbfb3d7fa6db
stephcmartin <martin.stephaniechow@gmail.com>
2017-11-27 12:09:44 -0500

    Created basic CSS template
    - Added Nav Bar
    - Added Font styles + sizes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+let colours = ['rgb(255, 0, 0)', 'rgb(255, 255, 0)', 'rgb(0, 255, 0)',
+'rgb(0, 255, 255)', 'rgb(0, 0, 255)', 'rgb(255, 0, 255)']
 
+let squares = document.querySelectorAll('.square')
+for(i = 0; i < squares.length; i++){
+  squares[i].style.backgroundColor = colours[i]
+}
